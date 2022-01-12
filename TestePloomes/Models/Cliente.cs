e7b14d65356da00f1a445ef210c0db25bc39a37e
bauer1 <andrebb22@gmail.com>
2022-01-12 18:16:50 -0300

    nothing much

--- TestePloomes/Models/Cliente.cs
+++ TestePloomes/Models/Cliente.cs
@@ -7,7 +7,7 @@ namespace TestePloomes.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public string Id { get; set; } = "";
         public string NomeCompleto { get; set; }
         public string Nickname { get; set; }
         public bool Habilitado { get; set; }
